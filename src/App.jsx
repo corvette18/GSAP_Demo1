a5a2cabe45553f5bb22deb8a3a181cde042154f4
corvette18@outlook.com <corvette18@outlook.com>
2025-12-30 08:57:27 -0800

    Hero Section scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import gsap from 'gsap';
 import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
@@ -10,8 +11,11 @@ const App = () => {
     return (
    <main>
        <Navbar />
+       <Hero />
+       <div className="h-dvh bg-black"></div>
    </main>
     )
 }
+
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,79 @@
+import React, {useRef} from 'react'
+import {useGSAP} from "@gsap/react";
+import { SplitText} from "gsap/all";
+import gsap from "gsap";
+import {useMediaQuery} from "react-responsive";
+
+const Hero = () => {
+    const videoRef = useRef()
+    const isMobile = useMediaQuery({maxWidth: 767})
+    useGSAP(() => {
+
+
+const heroSplit = new SplitText('.title',{type:'chars,words'})
+        const paraSplit = new SplitText('.subtitle',{type:'lines'})
+
+        heroSplit.chars.forEach((char) => char.classList.add("text-gradient"))
+
+        gsap.from(heroSplit.chars, {yPercent: 100, duration: 1.8, ease:'expo.out', stagger: 0.06 })
+        gsap.from(paraSplit.lines,{opacity:0,yPercent:100,duration:1.8, ease:'expo.out', stagger: 0.06, delay:1})
+
+        gsap.timeline({ scrollTrigger: {
+            trigger: "#hero",
+                start: "top top",
+                end: 'bottom top',
+                scrub: true
+            }})
+            .to('.right-leaf',{y: 200},0)
+            .to('.left-leaf',{y: -200},0)
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue =  isMobile ? '120% top' : 'bottom top';
+
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
+
+    },[])
+
+    return <>
+ <section className="noisy" id="hero">
+     <h1 className="title">Mojito</h1>
+     <img src="/images/hero-left-leaf.png" alt="left-leaf"
+className="left-leaf"
+          />
+     <img src="/images/hero-right-leaf.png" alt="right-leaf"
+          className="right-leaf"
+     />
+     <div className="body">
+         <div className="content">
+            <div className="space-y-5 hidden md:block">
+                <p>cool. crisp, classic</p>
+                <p className="subtitle">
+                    sip the spirit <br /> of summer
+                </p>
+            </div>
+         <div className="view-cocktails"><p className="sub-title"> every cocktail has cool stuff </p>
+         <a href="#cocktails">view Cocktails</a></div>
+     </div>
+     </div>
+ </section>
+        <div className="video absolute inset-0">
+            <video src="/videos/output.mp4" muted playsInline preload="auto" id="video1" ref={videoRef} />
+
+        </div>
+        </>
+}
+export default Hero
